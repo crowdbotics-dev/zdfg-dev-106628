b45f048ecd57e917fd448d0a3cc764ee843834d8
Crowdbotics <engineering@crowdbotics.com>
2023-08-02 17:03:36 +0000

    Initial commit.

---
+++ backend/zdfg_dev_106628/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zdfg_dev_106628 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zdfg_dev_106628.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "zdfg-dev-106628",
+  url: "https://zdfg-dev-106628.botics.co",
+  api: "https://zdfg-dev-106628.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
